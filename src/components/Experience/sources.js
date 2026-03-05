213cc74b26928e3d863e2a2428a90ddb5bab4cd6
nehemiatelusma <nehemia.telusma@akqa.com>
2026-03-05 16:35:27 +0100

    file: images added

--- src/components/Experience/sources.js
+++ src/components/Experience/sources.js
@@ -1,26 +1,7 @@
-export default [
-  // Put the files in the Public folder
-  // {
-  //   name: "environmentMapTexture",
-  //   type: "cubeTexture",
-  //   path: [
-  //     "/textures/environmentMap/px.jpg",
-  //     "/textures/environmentMap/nx.jpg",
-  //   ],
-  // },
-  // {
-  //   name: "grassColorTexture",
-  //   type: "texture",
-  //   path: "/textures/dirt/color.jpg",
-  // },
-  // {
-  //   name: "grassNormalTexture",
-  //   type: "texture",
-  //   path: "/textures/dirt/normal.jpg",
-  // },
-  // {
-  //   name: "foxModel",
-  //   type: "gltf",
-  //   path: "/models/Fox/glTF/Fox.gltf",
-  // },
-];
+import listData from "../../data/listData.js";
+
+export default listData.map((item) => ({
+  name: `galleryTexture${item.id}`,
+  type: "texture",
+  path: item.image,
+}));
