6c3fcade21c5d3859093a1b0ec1b15939a10e99d
nehemiatelusma <nehemia.telusma@akqa.com>
2026-03-23 09:54:26 +0100

    fix: navbar + current tab

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 import LoopIcon from "./icons/loopIcon";
 
 const NavBar = () => {
+
+    const variants = {
+        active: "font-bold font-canal-demi-romain text-accent transition-colors duration-300",
+        inactive: "font-medium font-canal-demi-romain hover:text-accent transition-colors duration-300",
+    }
   return (
     <div className="fixed top-0 left-0 w-full z-50 flex items-center justify-between px-4 md:px-8 py-2">
         <nav className="flex items-center space-x-4 md:space-x-6">
@@ -11,20 +16,23 @@ const NavBar = () => {
             </div>
 
             <ul className="hidden md:flex items-center space-x-4">
-                <li className="font-medium hover:text-accent transition-colors duration-300">
+                <li className={variants.inactive}>
                     <Link href="/" className="font-canal-demi-romain">Accueil</Link>
                 </li>
-                <li className="font-medium font-canal-demi-romain hover:text-accent transition-colors duration-300">
+                <li className={variants.inactive}>
                     <Link href="/">En direct</Link>
                 </li>
-                <li className="font-bold font-canal-demi-romain hover:text-accent transition-colors duration-300">
+                <li className={variants.inactive}>
                     <Link href="/">Programme TV</Link>
                 </li>
-                <li className="font-medium font-canal-demi-romain hover:text-accent transition-colors duration-300">
+                <li className={variants.inactive}>
                     <Link href="/">Chaînes & Apps</Link>
                 </li>
-                <li className="font-medium font-canal-demi-romain hover:text-accent transition-colors duration-300">
+                <li className={variants.inactive}>
                     <Link href="/">Mes vidéos</Link>
+                </li>
+                <li className={variants.active}>
+                    <Link href="/">Originals+1</Link>
                 </li>
             </ul>
         </nav>
