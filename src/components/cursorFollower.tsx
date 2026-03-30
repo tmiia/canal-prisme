b9085fdd1ab4e278d714cc918bebbe580a20cceb
nehemiatelusma <nehemia.telusma@akqa.com>
2026-03-30 11:38:19 +0200

    fix(ui): cursor follower

--- src/components/cursorFollower.tsx
+++ src/components/cursorFollower.tsx
@@ -9,7 +9,7 @@ export default function CursorFollower() {
   useEffect(() => {
     const onPointerMove = (e: PointerEvent) => {
       if (spanRef.current) {
-        spanRef.current.style.transform = `translate(${e.clientX + 12}px, ${e.clientY + 12}px) transition-all duration-300 ease-in-out`;
+        spanRef.current.style.transform = `translate(${e.clientX + 12}px, ${e.clientY + 12}px)`;
       }
     };
 
@@ -30,7 +30,7 @@ export default function CursorFollower() {
   return (
     <span
       ref={spanRef}
-      className="fixed top-0 left-0 pointer-events-none bg-black text-white text-xs p-1 px-1.5 z-50 font-canal-light-italic hidden md:block"
+      className="fixed top-0 left-0 pointer-events-none bg-black text-white text-xs p-1 px-1.5 z-50 font-canal-light-italic hidden md:block transition-transform duration-100 ease-out"
     >
       {isHovering ? "[ Click ]" : "[ Scroll ]"}
     </span>
